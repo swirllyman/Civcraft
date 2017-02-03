e20e0d687ebec5afb688c9c3f0cd938c5359729f
swirllyman <swirllyman@gmail.com>
2017-02-02 19:17:41 -0800

    Initial network framework

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class BasicMovement : NetworkBehaviour {
+
+    public float moveSpeed = 1.0f;
+    Camera myCam;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            myCam = GetComponentInChildren<Camera>();
+        }
+        else
+        {
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+
+        Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            ColorChanger c = hit.transform.GetComponent<ColorChanger>();
+            if (c != null && Input.GetMouseButtonDown(0))
+            {
+                CmdChangeObjectColor(c.gameObject);
+            }
+        }
+    }
+
+    [Command]
+    void CmdChangeObjectColor(GameObject g)
+    {
+        g.GetComponent<ColorChanger>().CmdNextColor();
+    }
+}
---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ColorChanger : NetworkBehaviour {
+
+    public Color[] colors;
+    Renderer rend;
+
+    [SyncVar]
+    public int colorIdx = 0;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+	public void NextColor()
+    {
+        if (isServer)
+        {
+            CmdNextColor();
+        }
+    }
+
+    [Command]
+    public void CmdNextColor()
+    {
+        RpcNextColor();
+    }
+
+    [ClientRpc]
+    void RpcNextColor()
+    {
+        int idx = (colorIdx++) % colors.Length;
+        rend.material.color = colors[idx];
+    }
+}
