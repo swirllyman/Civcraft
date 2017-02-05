77a58672a83e01e2672af1840ce76a65a8b72d5e
swirllyman <swirllyman@gmail.com>
2017-02-04 16:35:56 -0800

    color coating

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections;
 
 public class EnemySpawner : NetworkBehaviour
 {
-
     public GameObject enemyPrefab;
     public int numberOfEnemies;
 
+    public float spawnTimer = 5.0f;
     void Start()
     {
         if (!isServer) return;
+        StartCoroutine(SpawnAfterTime());
+    }
+
+    IEnumerator SpawnAfterTime()
+    {
+        yield return new WaitForSeconds(spawnTimer);
         for (int i = 0; i < numberOfEnemies; i++)
         {
             var spawnPosition = gameObject.transform.position + new Vector3(Random.Range(-8.0f, 8.0f), 0.0f, Random.Range(-8.0f, 8.0f));
@@ -18,7 +25,19 @@ public class EnemySpawner : NetworkBehaviour
 
             var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
             NetworkServer.Spawn(enemy);
-            
+            CmdSetupUnit(enemy);
         }
+    }
+
+    [Command]
+    void CmdSetupUnit(GameObject g)
+    {
+        RpcSetupUnit(g);
+    }
+
+    [ClientRpc]
+    void RpcSetupUnit(GameObject g)
+    {
+        g.GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
     }
 }
